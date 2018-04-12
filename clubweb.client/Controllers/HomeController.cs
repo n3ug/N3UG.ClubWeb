c7a2dab989a85a82961e34146c2e36cabf2dabf4
albert18 <caps18@hotmail.com>
2018-04-12 12:31:51 -0400

    Add Contact Form in contact page

--- clubweb.client/Controllers/HomeController.cs
+++ clubweb.client/Controllers/HomeController.cs
@@ -8,6 +8,9 @@ using clubweb.client.Models;
 using clubweb.shared.models.user;
 using System.Net.Http;
 using RestSharp;
+using System.Net.Mail;
+using System.Net;
+using clubweb.shared.models.contacts;
 
 namespace clubweb.client.Controllers
 {
@@ -27,9 +30,52 @@ namespace clubweb.client.Controllers
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
+        }
+
+        /// <summary>
+        /// Author: Albert Guerrero from Er-Er Tech - Able to send email using SMTP server
+        /// Note: This is only for gmail 
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Contact(Contact model)
+        {
+            try
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("EMAILHERE@gmail.com")); //-- Put your email gmail email address
+                message.From = new MailAddress("EMAILHERE@gmail.com"); //-- Put your email gmail email address
+                message.Subject = "N3UG.ClubWeb Question";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "EMAILHERE@gmail.com", //-- Put your email gmail email address
+                        Password = "PASSWORDHERE" //-- Put your email gmail Password
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    //Success
+                    ViewBag.Status = true;
+                }
+            }
+            //Fails
+            catch (Exception)
+            {
+                ViewBag.Status = "Problem while sending email, Please check details.";
+            }
+
+            //return model to Contact.cshtml
+            return View(model);
         }
 
         public IActionResult Error()
